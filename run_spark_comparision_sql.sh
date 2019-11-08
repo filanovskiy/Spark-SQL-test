07fab8503e47ba73934a245c1ba39363d574ba86
filanovskiy <a.filanovskiy@gmail.com>
2019-11-08 14:56:14 -0800

    fix

--- run_spark_comparision_sql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#nohup spark2-shell -i spark-sql-tpcds.scala.short --properties-file /root/fil/spark-sql/spark-defaults_v2.conf &
-#sleep 5400
-#nohup spark2-shell -i spark-sql-tpcds.scala.short --properties-file /root/fil/spark-sql/spark-defaults_v1.conf & 
--- run_spark_sql.sh
+++ run_spark_sql.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nohup time spark2-shell -i spark-sql-tpcds-short.scala --properties-file ./spark-defaults_v2.conf &
+nohup time spark-shell -i spark-sql-tpcds-short.scala  &
+#nohup time spark2-shell -i spark-sql-tpcds-short.scala --properties-file ./spark-defaults_v2.conf &
 #nohup time spark2-shell -i spark-sql-tpcds-full.scala --properties-file ./spark-defaults_v2.conf &
