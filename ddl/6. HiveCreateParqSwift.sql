f28888337823cf107f70a8734157768d1e243f38
filanovskiy <a.filanovskiy@gmail.com>
2019-11-08 14:14:08 -0800

    Update 6. HiveCreateParqSwift.sql

--- ddl/6. HiveCreateParqSwift.sql
+++ ddl/6. HiveCreateParqSwift.sql
@@ -4,9 +4,9 @@
 --The source code contained or described herein and all documents related to the source code ("Material") are owned by Intel Corporation or its suppliers or licensors. Title to the Material remains with Intel Corporation or its suppliers and licensors. The Material contains trade secrets and proprietary and confidential information of Intel or its suppliers and licensors. The Material is protected by worldwide copyright and trade secret laws and treaty provisions. No part of the Material may be used, copied, reproduced, modified, published, uploaded, posted, transmitted, distributed, or disclosed in any way without Intel's prior express written permission.
 --
 --No license under any patent, copyright, trade secret or other intellectual property right is granted to or conferred upon you by disclosure or delivery of the Materials, either expressly, by implication, inducement, estoppel or otherwise. Any license under such intellectual property rights must be express and approved by Intel in writing.
-create database parq_swift;
-use  parq_swift; 
-set hdfsDataPath= swift://tpcds-parq.bdcstorage;
+create database parq_oci;
+use  parq_oci; 
+set hdfsDataPath= oci://TCPDSPARQ@oraclebigdatadb/;
 set fieldDelimiter=|;
 
 set customerTableName=customer;
