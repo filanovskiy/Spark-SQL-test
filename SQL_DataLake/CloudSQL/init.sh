99ec93f057247b3dbca28f9cf75bea1cdb49b82c
a.filanovskiy@gmail.com <a.filanovskiy@gmail.com>
2020-10-05 21:58:39 -0700

    MVP version

--- SQL_DataLake/CloudSQL/init.sh
+++ SQL_DataLake/CloudSQL/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql_dba.sh create_bdsuser.sql 
-/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql.sh drop_tables.sql 
-/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql.sh create_os_cred.sql 
-/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql.sh create_tables.sql 
+/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql_dba.sh /home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/create_bdsuser.sql 
+/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql.sh /home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/drop_tables.sql 
+/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql.sh /home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/create_os_cred.sql 
+/home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/run_sql.sh /home/oracle/Spark-SQL-test/SQL_DataLake/CloudSQL/create_tables.sql 
 
